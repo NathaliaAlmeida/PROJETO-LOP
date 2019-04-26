56a91a65657ea2a3a096010c26cfa4590efb2044
NathaliaAlmeida <nathaliamc.almeida@gmail.com>
2019-04-26 14:34:03 -0300

    Create Etapa1.js

---
+++ Etapa1.js
@@ -0,0 +1,41 @@
+/* Equipe:
+  Erich Brendell Araújo Medeiros
+  Nathalia Medeiros Costa de Almeida
+  Etapa 1
+*/
+ 
+var x = 200;
+var y = 350;
+
+function setup() {
+  createCanvas(400, 400)
+  fill('yellow')
+ 
+}
+
+function draw() {
+  background('black');
+  rect(389, 1, 10 , 397);
+  rect(0, 1, 10, 397);
+  rect(10, 1, 379, 10);
+  rect(10, 388, 379, 10);
+  rect(200, 100, 35, 35);
+  
+  
+  ellipse(x,y, 40, 40);
+   if (keyIsDown(RIGHT_ARROW)) {
+    x = x + 3;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    x = x - 3;
+  }
+   if (keyIsDown(DOWN_ARROW)) {
+    y = y + 2;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    y = y - 2;
+  }
+ 
+  
+  
+}
