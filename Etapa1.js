51c5161ab1ec84e4f664699acb03f008f80cd25b
NathaliaAlmeida <nathaliamc.almeida@gmail.com>
2019-05-11 21:06:51 -0300

    Update Etapa1.js

--- Etapa1.js
+++ Etapa1.js
@@ -1,26 +1,46 @@
 /* Equipe:
-  Erich Brendell Araújo Medeiros - Subturma 01A
-  Nathalia Medeiros Costa de Almeida - Subturma 01A
-  Etapas 1 e 2
+  Erich Brendell Araújo Medeiros
+  Nathalia Medeiros Costa de Almeida
+  Etapa 1 e 2
+  Etapa 3 
 */
  
 var x = 200;
 var y = 350;
+var z = 38;
+var s = 26;
+var a = 810;
+var b = 26;
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(1000, 690)
   fill('yellow')
  
 }
 
 function draw() {
   background('black');
-  rect(389, 1, 10 , 397);
-  rect(0, 1, 10, 397);
-  rect(10, 1, 379, 10);
-  rect(10, 388, 379, 10);
-  rect(200, 100, 35, 35);
-  
+  rect(844, 1, 10 , 687);
+  rect(0, 1, 10, 687);
+  rect(10, 1, 834, 10);
+  rect(10, 678, 834, 10);
+  rect(55, 40, 70, 40, 10);
+  rect(175, 40, 100, 40, 10);
+  rect(325, 40, 70, 40, 10);
+  rect(440, 11, 25, 70, 3);
+  rect(510, 40, 70, 40, 10);
+  rect(615, 40, 100, 40, 10);
+  rect(750, 40, 70, 40, 10);
+  circle(z, s, 10);
+  z = z + 1.75
+  if(z > 413){
+   z = 0
+  }
+  circle(a, b, 10);
+  a = a - 1.55
+  if (a < 473){
+   a = 800
+  }
   
   ellipse(x,y, 40, 40);
    if (keyIsDown(RIGHT_ARROW)) {
@@ -36,6 +56,5 @@ function draw() {
     y = y - 2;
   }
  
-  
   
 }
