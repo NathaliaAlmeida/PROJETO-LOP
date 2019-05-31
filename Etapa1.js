eab880270f13328c6ebb11bc344480abec28110b
NathaliaAlmeida <nathaliamc.almeida@gmail.com>
2019-05-30 22:05:18 -0300

    Update Etapa1.js

--- Etapa1.js
+++ Etapa1.js
@@ -2,59 +2,119 @@
   Erich Brendell Araújo Medeiros
   Nathalia Medeiros Costa de Almeida
   Etapa 1 e 2
-  Etapa 3 
+  Etapa 3 e 4
+  Etapa 5 e 6
 */
  
-var x = 200;
-var y = 350;
-var z = 38;
-var s = 26;
-var a = 810;
-var b = 26;
+var xJog = 500;
+var yJog = 400;
+var x1 = 38;
+var y1 = 26;
+var x2 = 810;
+var y2 = 26;
+var raioP = 15;
+var raioO = 15;
+var t;
+var vxo = [];
+var vyo = [];
+var qtObjetos = 20;
+var vVel = [];
+var vidas = 3;
+var pontos = 0;
+var nivel = 1;
+var tempo = 90;
 
 function setup() {
-  createCanvas(1000, 690)
+  createCanvas(1200, 690)
   fill('yellow')
- 
 }
 
 function draw() {
-  background('black');
-  rect(844, 1, 10 , 687);
+  //Apresentação de informações na tela de jogo
+  background(0);
+  textSize(32);
+  text('Vidas: ' + vidas, 980, 70);
+  fill('yellow');
+  textSize(32);
+  text('Tempo: ' + tempo, 980, 110);
+  textSize(32);
+  text('Nível:' + nivel, 981, 30);
+  textSize(32);
+  text('Pontuação: ' + pontos, 981, 150);
+
+  //Labirinto
+  rect(60, 50, 130, 90);
+  rect(250, 50, 160, 90);
+  rect(465, 10, 20, 130);
+  rect(540, 50, 160, 90);
+  rect(760, 50, 130, 90);
+  rect(60, 195, 130, 40);
+  rect(250, 195, 50, 150);
+  rect(360, 195, 50, 110);
+  rect(360, 305, 230, 40);
+  rect(540, 195, 50, 110);
+  rect(650, 195, 50, 150);
+  rect(760, 195, 130, 40);
+  rect(60, 300, 130, 40);
+  rect(760, 300, 130, 40);
+  rect(250, 400, 450, 50);
+  rect(60, 400, 130, 90);
+  rect(760, 400, 130, 90);
+  rect(60, 540, 40, 100);
+  rect(150, 540, 40, 100);
+  rect(760, 540, 40, 100);
+  rect(850, 540, 40, 100);
+  rect(250, 500, 40, 140);
+  rect(660, 500, 40, 140);
+  rect(350, 500, 250, 140);
+  
+  //Criação das paredes laterais
+  rect(950, 1, 10 , 687);
   rect(0, 1, 10, 687);
-  rect(10, 1, 834, 10);
-  rect(10, 678, 834, 10);
-  rect(55, 40, 70, 40, 10);
-  rect(175, 40, 100, 40, 10);
-  rect(325, 40, 70, 40, 10);
-  rect(440, 11, 25, 70, 3);
-  rect(510, 40, 70, 40, 10);
-  rect(615, 40, 100, 40, 10);
-  rect(750, 40, 70, 40, 10);
-  circle(z, s, 10);
-  z = z + 1.75
-  if(z > 413){
-   z = 0
+  rect(10, 1, 940, 10);
+  rect(10, 678, 940, 10);
+  
+  //Objetos andando ao longo do cenário
+  circle(x1, y1, 10);
+  x1 = x1 + 1.75;
+  if(x1 > 800){
+    x1 = 810;
+    if( y1 < 300)
+      y1 = y1 + 1.75;
+    else{
+      y1 = 30;
+      x1 = 10;
+    }
   }
-  circle(a, b, 10);
-  a = a - 1.55
-  if (a < 473){
-   a = 800
+  circle(x2, y2, 10);
+  x2 = x2 - 1.55;
+  if (x2 < 473){
+   x2 = 800;
   }
   
-  ellipse(x,y, 40, 40);
+  //Movimentação do jogador
+  ellipse(xJog,yJog, 2*raioP, 2*raioO);
    if (keyIsDown(RIGHT_ARROW)) {
-    x = x + 3;
+    xJog = xJog + 3;
   }
   if (keyIsDown(LEFT_ARROW)) {
-    x = x - 3;
+    xJog = xJog - 3;
   }
    if (keyIsDown(DOWN_ARROW)) {
-    y = y + 2;
+    yJog = yJog + 3;
   }
   if (keyIsDown(UP_ARROW)) {
-    y = y - 2;
+    yJog = yJog - 3;
   }
- 
-  
+  if( dist(xJog, yJog, x1, y1) < raioP + raioO ){
+    xJog = 400;
+    yJog = 380;
+    vidas--;
+  }
+  if( dist(xJog, yJog, x2, y2) < raioP + raioO ){
+    xJog = 400;
+    yJog = 380;
+    vidas--;
+  }
 }
+
